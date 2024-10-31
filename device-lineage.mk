413a5fcc9bca6ee686e0f4735fb9e9b25802a580
Ethan <e.slimerancher@gmail.com>
2024-10-31 20:23:20 +1300

    Initial bringup

--- device-lineage.mk
+++ device-lineage.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Disable Artifact Requirements
+DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
@@ -21,6 +24,19 @@ PRODUCT_PACKAGES := \
 
 # Kernel
 TARGET_PREBUILT_KERNEL := device/google/bluejay-kernel/Image.lz4
+
+# OrionOS flags
+ORION_MAINTAINER := SirEthanator
+ORION_BUILD_TYPE := UNOFFICIAL
+TARGET_ENABLE_BLUR := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_HAS_UDFPS := true
+
+# Gapps
+ORION_GAPPS := true
+BUILD_GOOGLE_CONTACTS := true
+BUILD_GOOGLE_DIALER := true
+BUILD_GOOGLE_MESSAGE := true
 
 # Build necessary packages for vendor
 
