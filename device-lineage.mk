a246883dd26b56eeca4fca536d2cd763740b3de9
Ethan <e.slimerancher@gmail.com>
2024-11-27 19:35:17 +1300

    Re-enable BUILD_BROKEN_MISSING_REQUIRED_MODULES

--- device-lineage.mk
+++ device-lineage.mk
@@ -7,6 +7,9 @@
 # Scudo
 PRODUCT_USE_SCUDO := true
 
+# Ignore missing modules
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
+
 # Disable Artifact Requirements
 DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
 
